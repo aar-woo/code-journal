3d082ba7f6ab4b94ac9f0b0be79fbac0a6563f27
Aaron Woo <awoo@chapman.edu>
2021-09-08 10:36:19 -0700

    Changed to using localStorage directly, moved all localStorage code to data.js an copied whole previousDataObj rather than each property.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,17 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataObj = localStorage.getItem('local-data-object');
+
+if (previousDataObj.entries !== null) {
+  var previousData = JSON.parse(previousDataObj);
+  data = previousData;
+}
+
+function unload(event) {
+  var dataObjJSON = JSON.stringify(data);
+  localStorage.setItem('local-data-object', dataObjJSON);
+}
+
+window.addEventListener('beforeunload', unload);
--- js/main.js
+++ js/main.js
@@ -26,22 +26,3 @@ function onSubmit(event) {
 }
 
 $entryForm.addEventListener('submit', onSubmit);
-
-var $myStorage = window.localStorage;
-
-var previousDataObj = $myStorage.getItem('local-data-object');
-
-if (previousDataObj.entries !== null) {
-  var previousData = JSON.parse(previousDataObj);
-  data.entries = previousData.entries;
-  data.view = previousData.view;
-  data.editing = previousData.editing;
-  data.nextEntryId = previousData.nextEntryId;
-}
-
-function unload(event) {
-  var dataObjJSON = JSON.stringify(data);
-  $myStorage.setItem('local-data-object', dataObjJSON);
-}
-
-window.addEventListener('beforeunload', unload);
